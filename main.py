421b55e197bbfafafb7156b6867aecc0238b27bb
Ali Rostami <rostami@axxessio.com>
2022-05-07 09:55:01 +0200

    generates first version of data

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import csv
+import csv, random
 
 words = []
 with open("persian-wikipedia.csv") as f:
@@ -8,7 +8,20 @@ with open("persian-wikipedia.csv") as f:
             words.append(row[0])
 
 
-
+dict = {}
 with open("dict.csv") as f:
-    reader = csv.reader(f, delimiter='\t')
+    reader = csv.reader(f, delimiter=',')
     for row in reader:
+        dict[row[1]] = row[0]
+
+# print(dict)
+
+with open("prepared_data.csv", "w") as f:
+    writer = csv.writer(f)
+    for w in words:
+        if w in dict.keys():
+            i = random.randint(0, len(words) - 1)
+            j = random.randint(0, len(words) - 1)
+            k = random.randint(0, len(words) - 1)
+            writer.writerow([w, dict[w], words[i], words[j], words[k]])
+
