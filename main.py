9debdec8de970fe01a747a96ae6e95c294623be8
Ali Rostami <rostami@axxessio.com>
2022-05-08 16:23:06 +0200

    last version

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import csv, random
+import csv, random, json
+from itertools import permutations
 
 words = []
 with open("persian-wikipedia.csv") as f:
@@ -7,21 +8,68 @@ with open("persian-wikipedia.csv") as f:
         if int(row[1]) > 2000:
             words.append(row[0])
 
-
-dict = {}
+dict_en_fa = {}
+dict_fa_en = {}
+words_english = []
 with open("dict.csv") as f:
     reader = csv.reader(f, delimiter=',')
     for row in reader:
-        dict[row[1]] = row[0]
+        dict_en_fa[row[1]] = row[0]
+        dict_fa_en[row[0]] = row[1]
+        words_english = row[1]
+
 
 # print(dict)
 
+data = []
 with open("prepared_data.csv", "w") as f:
-    writer = csv.writer(f)
-    for w in words:
-        if w in dict.keys():
-            i = random.randint(0, len(words) - 1)
-            j = random.randint(0, len(words) - 1)
-            k = random.randint(0, len(words) - 1)
-            writer.writerow([w, dict[w], words[i], words[j], words[k]])
+    with open("words_questions.json", "w") as f2:
+        writer = csv.writer(f)
+        for w in words:
+            if w in dict_en_fa.keys():
+                ind = [i,j,k] = random.sample(range(0, len(words) - 1), 3)
+                writer.writerow([w, dict_en_fa[w], words[i], words[j], words[k]])
 
+                perm = permutations([0, 1, 2, 3])
+                choices = []
+                d = {}
+                cnt = 0
+                answer = 0
+                for i in list(perm)[0]:
+                    if i == 3:
+                        choices.append(w)
+                        answer = cnt
+                    else:
+                        choices.append(words[ind[i]])
+                    cnt = cnt + 1
+                data.append({
+                    "question": dict_en_fa[w],
+                    "choices": choices,
+                    "answer": answer
+                })
+
+        for w in words_english:
+            if w in dict_fa_en.keys():
+                ind = [i, j, k] = random.sample(range(0, len(words_english) - 1), 3)
+                writer.writerow([w, dict_fa_en[w], words_english[i], words_english[j], words_english[k]])
+
+                perm = permutations([0, 1, 2, 3])
+                choices = []
+                d = {}
+                cnt = 0
+                answer = 0
+                for i in list(perm)[0]:
+                    if i == 3:
+                        choices.append(w)
+                        answer = cnt
+                    else:
+                        choices.append(words[ind[i]])
+                    cnt = cnt + 1
+
+                data.append({
+                    "question": dict_fa_en[w],
+                    "choices": choices,
+                    "answer": answer
+                })
+
+        json.dump(data, f2, indent=2, ensure_ascii=False)
